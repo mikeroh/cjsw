851019837447bd885310bae2ade0f07774b61a96
Michael Rohs <michaelgregory.rohs@gmail.com>
2016-04-17 11:07:38 -0600

    Preserve original path when creating show path.

--- scrape.py
+++ scrape.py
@@ -56,7 +56,7 @@ print "\n\n0 : All"
 print_options(shows)
 print "\nSelect a show date from the list above."
 print "(Enter number 0 - " + str(len(shows)) + ") :"
-path = str(pathlib.Path(str(path) + "/" + genre.rsplit('/')[-2] + "/" + program.rsplit("/")[-2]))
+show_path = str(pathlib.Path(str(path) + "/" + genre.rsplit('/')[-2] + "/" + program.rsplit("/")[-2]))
 
 try:
     x = int(stdin.readline())
@@ -68,11 +68,11 @@ except ValueError:
 if (x > 0) and (x < len(shows)+1):
     info = get_info(shows[x-1], program)
     if(info[0]):
-        download(info[2], path, info[1], shows[x-1])
+        download(info[2], show_path, info[1], shows[x-1])
 elif (x == 0):
     for show in shows:
         info = get_info(show, program)
         if(info[0]):
-            download(info[2], path, info[1], show)
+            download(info[2], show_path, info[1], show)
 else:
     exit()
